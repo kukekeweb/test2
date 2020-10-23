a4d32e0f1c65e1cf178b998fc2efaa7a26f7903c
kukekeweb <73024063+kukekeweb@users.noreply.github.com>
2020-10-23 22:41:48 +0900

    Update my2.js

--- my2.js
+++ my2.js
@@ -1,23 +1,9 @@
+function play_se2(){
+    var おばけのウケねらい = new Audio('おばけのウケねらい.mp3');
+    おばけのウケねらい.play();
+}
 
 
-
-//音声の再生
-document.getElementById('audio').play();
-
-//音声の一時停止
-document.getElementById('audio').pause();
-$(function () {
-  var audioBtn = $('.audio_button'),
-  audioWrap = $('.audio_wrap'),
-  audio = document.getElementById('audio');
-
-  audioBtn.on('click', function () {
-    if( audioWrap.hasClass('play') ) {
-      audio.pause();
-      audioWrap.removeClass('play');
-    } else {
-      audio.play();
-      audioWrap.addClass('play');
-    }
-  });
+$(function(){
+    $('#2曲目').click(play_se2);
 });
